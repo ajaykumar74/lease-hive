02cac55e42c6b9f40a9401087d383525367f0d28
ajay <ajay@vloot.in>
2026-09-05 19:36:08 +0530

    lease contract uipdated

--- src/app/views/leaseContracts/leaseContractStatus/leaseContractStatus-create.component.ts
+++ src/app/views/leaseContracts/leaseContractStatus/leaseContractStatus-create.component.ts
@@ -146,7 +146,6 @@ RecordStatus: obj.RecordStatus || '',
   
 	const formValues  = this.editForm.value ;
 	var createdObj = { 
-      TenantId: this.loggedInUserService.loggedInUser.Tenant.Id,
       Id: this.objMaster.Id,
       RowVersionStr : this.objMaster.RowVersionStr,
      StatusCode: formValues.StatusCode || null,
